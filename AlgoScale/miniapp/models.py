189a110430ffddead4cb0874e50ac51a536487d3
rishabh017 <rishabhsharmauk@gmail.com>
2020-08-09 15:15:15 +0530

    Added Templates and User Model

--- AlgoScale/miniapp/models.py
+++ AlgoScale/miniapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.PositiveIntegerField(max_length=200)
+    hometown=models.CharField(max_length=200)
